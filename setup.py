eccf80ea3a8337b18a0dd1d892219b1bb044909a
Gungor Budak <gbudak@149-166-112-111.dhcp-in.iupui.edu>
2015-11-06 15:17:40 -0500

    remove pyc and add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+EXCLUDE_FROM_PACKAGES = []
+
+setup(
+    name='gsea_rbp',
+    version='0.0.1',
+    description='Inferring associations between RNA-binding proteins (RBPs) and gene sets based on binding signals.',
+    url='https://bitbucket.org/gungorbudak/gsea_rbp/',
+    author='Gungor Budak',
+    author_email='gbudak@iupui.edu',
+    license='MIT',
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    zip_safe=False
+    )
